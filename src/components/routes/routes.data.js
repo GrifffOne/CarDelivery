a79aa92f12ea71a5d56ea1c5d4f0e7da14511c75
Evgeniy <rpufff6@mail.ru>
2024-08-06 13:33:01 +0300

    first commit

--- src/components/routes/routes.data.js
+++ src/components/routes/routes.data.js
@@ -2,22 +2,17 @@ import About from '../screen/about/About'
 import Cases from '../screen/cases/Cases'
 import Home from '../screen/home/Home'
 
-
-
 export const routes = [
 	{
 		path: '/',
 		component: Home,
-
 	},
 	{
 		path: '/cases',
 		component: Cases,
-
 	},
 	{
 		path: '/about',
 		component: About,
-		
 	},
 ]
